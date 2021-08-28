578baaa6ebe003517d07d6c3d9c0bcdca53e85db
CleytonPinheiro <cleyton_pin@hotmail.com>
2021-08-28 17:35:05 -0300

    Trabalhando com variaveis universal do projeto

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,9 +7,10 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { HeaderComponent } from "./components/templates/header/header.component";
 
 import { MatToolbarModule } from "@angular/material/toolbar";
+import { FooterComponent } from './components/template/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent],
   imports: [
     MatToolbarModule,
     BrowserModule,
