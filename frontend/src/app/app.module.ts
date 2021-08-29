d7adbec13e0481df26faa59dfd3344109351a8af
CleytonPinheiro <cleyton_pin@hotmail.com>
2021-08-29 11:15:42 -0300

    Buscando dados do backend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -20,6 +20,7 @@ import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
+import { ProductReadComponent } from './components/product/product-read/product-read.component';
 
 @NgModule({
   declarations: [
@@ -31,6 +32,7 @@ import { MatInputModule } from "@angular/material/input";
     ProductCrudComponent,
     RedDirective,
     ProductCreateComponent,
+    ProductReadComponent,
   ],
   imports: [
     MatToolbarModule,
