fc61e02b2900a185c662054a27732e71e90d0021
CleytonPinheiro <cleyton_pin@hotmail.com>
2021-09-02 23:31:59 -0300

    Componente delete

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -28,6 +28,7 @@ import { MatSortModule } from "@angular/material/sort";
 import localPt from "@angular/common/locales/pt";
 import { registerLocaleData } from "@angular/common";
 import { ProductUpdateComponent } from "./components/product/product-update/product-update.component";
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
 
 registerLocaleData(localPt);
 
@@ -44,6 +45,7 @@ registerLocaleData(localPt);
     ProductReadComponent,
     ProductRead2Component,
     ProductUpdateComponent,
+    ProductDeleteComponent,
   ],
   imports: [
     MatToolbarModule,
